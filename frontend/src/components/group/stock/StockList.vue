dda78baf0178c72b2a694a5133e248eb900bf3fa
박수아 <xlalfskdl@naver.com>
2021-04-23 15:20:29 +0900

    [ADD] add stock item and request item html

---
+++ frontend/src/components/group/stock/StockList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row">
+    <div class="col-sm-6">
+      <StockItem/>
+    </div>
+  </div>
+</template>
+
+<script>
+import StockItem from './StockItem.vue'
+export default {
+  components: { StockItem },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/group/MainPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="content">
+    <div class="container-fluid p-0">
+      <Request/>
+    </div>
+  </main>
+</template>
+
+<script>
+import Request from '@/components/group/main/Request.vue'
+export default {
+  components: { Request },
+
+}
+</script>
+
+<style>
+
+</style>

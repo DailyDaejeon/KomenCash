c96c830d24239130af06f318f68054158e972572
Hyerim Jeong <hrjeong8@gmail.com>
2021-04-23 12:22:48 +0900

    [UPDATE] update group list page

--- frontend/src/views/group/GroupList.vue
+++ frontend/src/views/group/GroupList.vue
@@ -1,72 +1,30 @@
 <template>
-  <div class="container-fluid">
-    <!-- 1. OOO선생님의 그룹 리스트 입니다 -->
-    <div style="margin-top:13px;">
-      <span><h2 style="display:inline-block">{{userName}}</h2> 선생님의 그룹 리스트 입니다.</span>
-      <p>
-        관리를 원하는 그룹을 선택해서 해당 그룹을 관리하세요.
-      </p>
-    </div>
-
-    <!-- 2. 튜토리얼 -->
-    <div>
-      
-    </div>
-    <hr>
-    <!-- 3. 그룹 리스트 -->
-    <div class="group-list">
-      <div class="row"> <!-- class="row" -->
-        <div class="group-list-card"> <!-- class="col-sm-2" -->
-          <div class="card">
-            <!-- <div class="more-icon">
-              <font-awesome-icon :icon="['fas', 'ellipsis-v']" />
-            </div> -->
-            <div class="card-body" style="padding-top:60px;padding-bottom:60px;text-align:center">
-              <h1 class="mt-1 mb-3">{{groupName}}</h1> <!--  style="display:inline-block;margin-right:10px" -->
-              <span class="text-danger">{{totalCnt}}명</span><br>
-              <!-- <button>관리하기</button> -->
-            </div>
-          </div>
-        </div>
-        <div class="add-group-card" @click="addGroup()">
-          <div class="card">
-            <div class="card-body" style="padding-top:73px;padding-bottom:73px;text-align:center">
-              <h2 class="mt-1 mb-3" style="display:inline-block;margin-right:10px">그룹 추가하기</h2>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- <div class="row">
-        <div class="col-sm-4">
-          <div class="card">
-            <div class="card-body" style="padding-top:50px;padding-bottom:50px">
-              <h1 class="mt-1 mb-3" style="display:inline-block;margin-right:10px">그룹 추가하기</h1>
-            </div>
-          </div>
-        </div>
-      </div> -->
-    </div>
+  <div>
+    <GroupListForm :userInfo="userName" :groupList="groupList" />
   </div>
 </template>
 
 <script>
+import GroupListForm from "@/components/group/GroupListForm"
 export default {
   data() {
     return {
       userName:"박싸피",
-      groupName:"그룹명",
-      totalCnt:25,
+      groupList:[],
     }
   },
+  components: { GroupListForm },
   created() {
-    featchGroupInfo();
+    this.featchGroupInfo();
   },
   methods: {
     featchGroupInfo(){
+      this.groupList = [
+        {groupName:"그룹1", totalCnt:25},
+        {groupName:"그룹2", totalCnt:36},
+        {groupName:"그룹3", totalCnt:17}
+      ];
     },
-    addGroup(){
-      // this.$router.push('/group/addGroup');
-    }
   },
 }
 </script>

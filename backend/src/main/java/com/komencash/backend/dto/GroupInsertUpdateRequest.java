ed8b55035d74ed639738419b96c5665563c5b8ea
배상웅 <dentist312@naver.com>
2021-04-26 16:03:39 +0900

    [UPDATE] Group Delete

--- backend/src/main/java/com/komencash/backend/dto/GroupInsertUpdateRequest.java
+++ backend/src/main/java/com/komencash/backend/dto/GroupInsertUpdateRequest.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class GroupInsertUpdateRequest {
+    private int id;
     private String code;
     private String name;
     private String monetary_unit_name;

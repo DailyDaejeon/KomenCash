dfea8b644f08f6921db605c1cb56fa06921de2fa
박수아 <xlalfskdl@naver.com>
2021-04-27 13:23:03 +0900

    [ADD] modify baseUrl

--- frontend/src/api/config/interceptors.js
+++ frontend/src/api/config/interceptors.js
@@ -6,7 +6,7 @@ import router from '@/routes/index.js';
 export function setInterceptors() {
   // let ddddd = store.state.user.token;
   let instance = axios.create({
-    baseURL: process.env.VUE_APP_URL,
+    baseURL: 'http://k4b203.p.ssafy.io:8081/api',
     // CORS 방지코드
     // headers: {
     //   Authorization: store.state.user.token

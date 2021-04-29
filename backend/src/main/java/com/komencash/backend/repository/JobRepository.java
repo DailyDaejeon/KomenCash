092bcd9a381f345c7dcaf755b27ad0b0519ba11d
jaeseok-go <gojeasuk2@naver.com>
2021-04-29 15:47:05 +0900

    [ADD] job delete api

--- backend/src/main/java/com/komencash/backend/repository/JobRepository.java
+++ backend/src/main/java/com/komencash/backend/repository/JobRepository.java
@@ -1,6 +1,5 @@
 package com.komencash.backend.repository;
 
-import com.komencash.backend.dto.job.JobSelectResponse;
 import com.komencash.backend.entity.job.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +8,6 @@ import java.util.List;
 public interface JobRepository extends JpaRepository<Job, Integer> {
 
     List<Job> findByGroup_id(int groupId);
+
+    Job findByName(String jobName);
 }

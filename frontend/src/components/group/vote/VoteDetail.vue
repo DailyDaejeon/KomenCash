416e8c2a57924d252b825b2823c21267658d4ff6
Hyerim Jeong <hrjeong8@gmail.com>
2021-04-29 23:22:10 +0900

    [UPDATE] update vote detail page

--- frontend/src/components/group/vote/VoteDetail.vue
+++ frontend/src/components/group/vote/VoteDetail.vue
@@ -1,15 +1,125 @@
 <template>
-  <div>
-    voteDetail 페이지
-    {{id}}
-  </div>
+  <main class="content">
+    <div class="container-fluid p-0">
+      <div class="row mb-2 mb-xl-3">
+        <div class="col-auto d-sm-block w-100">
+          <h3 class="d-inline-block">[{{voteInfo.title}}] 투표 결과</h3>
+          <p class="d-inline-block float-right">투표 개최자 : {{voteInfo.studentNickname}}</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="vote-result">
+          <div class="card-header border" v-for="(item,index) in voteInfo.voteItemResultResponses" :key="item.id">
+            <p>{{item.content}}</p>
+            <k-progress 
+              status="success" 
+              type="line"
+              :percent="Number(getPercent(index))"
+            ></k-progress>
+          </div>
+        </div>
+      </div>
+      <!-- <div class="row">
+        <div class="w-100">
+          <table>
+            <tr>
+              <th>No.</th>
+              <th>항목</th>
+              <th>득표 수</th>
+            </tr>
+            <tr v-for="voteItem in voteInfo.voteItemResultResponses" :key="voteItem.id">
+              <td>{{voteItem.itemNum}}</td>
+              <td>{{voteItem.content}}</td>
+              <td>{{voteItem.resultCnt}}</td>
+            </tr>
+          </table>
+        </div>
+      </div> -->
+    </div>
+  </main>
 </template>
 
 <script>
 export default {
-  props: {
-    id: Number
-  }
+  data() {
+    return {
+      voteInfo: {
+        id: 1,
+        title: "반장선거",
+        content:"2021년 햇반의 반장선거를 실시합니다. 지지하는 입후보자에게 투표해주세요.",
+        studentId:1,
+        studentNickname:"박싸피",
+        voteAttendResponses: [
+          {
+            choiceItemNum: 1,
+            studentId: 1,
+            studentNickname: "고재석",
+          },
+          {
+            choiceItemNum: 2,
+            studentId: 2,
+            studentNickname: "박수아",
+          },
+          {
+            choiceItemNum: 3,
+            studentId: 3,
+            studentNickname: "배상웅",
+          },
+          {
+            choiceItemNum: 4,
+            studentId: 4,
+            studentNickname: "정혜림",
+          },
+        ],
+        voteItemResultResponses: [
+          {
+            id: 1,
+            content: "고재석",
+            itemNum: 1,
+            resultCnt: 1
+          },
+          {
+            id:2,
+            content: "박수아",
+            itemNum: 2,
+            resultCnt: 1
+          },
+          {
+            id:3,
+            content: "배상웅",
+            itemNum: 3,
+            resultCnt: 1
+          },
+          {
+            id:4,
+            content: "정혜림",
+            itemNum: 4,
+            resultCnt: 1
+          }
+        ]
+      }
+    }
+  },
+  props: ['id'],
+  created() {
+    this.fetchVoteInfo();
+  },
+  methods: {
+    fetchVoteInfo() {
+      window.alert("voteInfo 불러오는 api 여기에 만들어조");
+    },
+    getPercent(idx){
+
+      let totalNum = 0;
+      for (let index = 0; index < this.voteInfo.voteItemResultResponses.length; index++) {
+        totalNum += this.voteInfo.voteItemResultResponses[index].resultCnt;
+      }
+      console.log("받은 표 : ",this.voteInfo.voteItemResultResponses[idx])
+      const pers = (this.voteInfo.voteItemResultResponses[idx].resultCnt / totalNum) * 100;
+      console.log(typeof(pers)," ",pers);
+      return pers;
+    }
+  },
 }
 </script>
 
--- frontend/src/components/group/vote/VoteList.vue
+++ frontend/src/components/group/vote/VoteList.vue
@@ -3,7 +3,7 @@
     <div class="card vl-width">
       <div class="card-header">
         <div v-if="this.voteList.length != 0">
-          <table class="table table-hover my-0" v-for="vote in voteList" :key="vote.id">
+          <table class="table table-hover my-0">
             <thead>
               <tr>
                 <th>투표 명</th>
@@ -13,8 +13,8 @@
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td @click="goVoteDetail(vote.id)">{{vote.title}}</td>
+              <tr v-for="vote in voteList" :key="vote.id">
+                <td @click="goVoteDetail(vote.id)">{{vote.title}}</td> <!-- @click="goVoteDetail(vote.id)" -->
                 <td class="d-none d-table-cell">{{vote.studentNickname}}</td>
                 <td class="d-none d-table-cell">{{voteAttandRate}}%</td>
                 <td class="d-none d-table-cell ta-center" @click="removeVote"> <!-- remove(index) -->
@@ -71,7 +71,62 @@ export default {
           ]
         },
       ],
-      voteAttandRate: 15,
+      voteInfo: {
+        id: 1,
+        title: "반장선거",
+        content:"2021년 햇반의 반장선거를 실시합니다. 지지하는 입후보자에게 투표해주세요.",
+        studentId:1,
+        studentNickname:"박싸피",
+        voteAttendResponses: [
+          {
+            choiceItemNum: 1,
+            studentId: 1,
+            studentNickname: "고재석",
+          },
+          {
+            choiceItemNum: 2,
+            studentId: 2,
+            studentNickname: "박수아",
+          },
+          {
+            choiceItemNum: 3,
+            studentId: 3,
+            studentNickname: "배상웅",
+          },
+          {
+            choiceItemNum: 4,
+            studentId: 4,
+            studentNickname: "정혜림",
+          },
+        ],
+        voteItemResultResponses: [
+          {
+            id: 1,
+            content: "고재석",
+            itemNum: 1,
+            resultCnt: 1
+          },
+          {
+            id:2,
+            content: "박수아",
+            itemNum: 2,
+            resultCnt: 1
+          },
+          {
+            id:3,
+            content: "배상웅",
+            itemNum: 3,
+            resultCnt: 1
+          },
+          {
+            id:4,
+            content: "정혜림",
+            itemNum: 4,
+            resultCnt: 1
+          }
+        ]
+      },
+      voteAttandRate: 0,
     }
   },
   created() {
@@ -81,7 +136,7 @@ export default {
     // fetchVoteList() {
 
     // },
-    goVoteDetail(vid){
+    goVoteDetail(vid){ //vid
       this.$router.push({name: 'VoteDetail', params: {id:vid}});
     },
     removeVote(index){

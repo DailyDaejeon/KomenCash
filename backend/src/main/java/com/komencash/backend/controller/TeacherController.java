25d0f0a7677d1f9df1016d6c62f5b266ef575d8c
배상웅 <dentist312@naver.com>
2021-04-30 15:52:06 +0900

    [Add] Allow CORS Headers

--- backend/src/main/java/com/komencash/backend/controller/TeacherController.java
+++ backend/src/main/java/com/komencash/backend/controller/TeacherController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/teacher")
 public class TeacherController {
 

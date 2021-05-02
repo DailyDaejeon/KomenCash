70473973cc9ac70ce03e9fb10727c85d6bac14ff
박수아 <xlalfskdl@naver.com>
2021-05-02 14:41:30 +0900

    [ADD] teacher connect job api

--- frontend/src/components/group/job/JobRequestList.vue
+++ frontend/src/components/group/job/JobRequestList.vue
@@ -13,9 +13,27 @@
 
 <script>
 import RequestItem from '../main/RequestItem.vue'
+import { fetchJobRequestList, fetchJobResumeList } from '@/api/job'
+
 export default {
   components: { RequestItem },
-
+  data () {
+    return {
+      resumeList : [],
+      jobList : []
+    }
+  },
+  created() {
+    this.fetchRequestList()
+  },
+  methods: {
+    async fetchRequestList() {
+      const resume = await fetchJobResumeList(1);
+      const job = await fetchJobRequestList(1);
+      this.resumeList = resume.data;
+      this.jobList = job.data;
+    }
+  }
 }
 </script>
 

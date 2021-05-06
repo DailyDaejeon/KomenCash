7b3cee68c0ed5b764a1d1ba399b76217dcde84aa
박수아 <xlalfskdl@naver.com>
2021-05-06 09:38:48 +0900

    [ADD] teacher case api

---
+++ frontend/src/api/case.js
@@ -0,0 +1,25 @@
+import { setInterceptors } from './config/interceptors'
+const instance = setInterceptors()
+
+// 사건경위서 목록 조회
+function fetchCaseList(groupId) {
+  return instance.get(`case/list/${groupId}`)
+}
+
+// 사건경위서 상세 조회
+function fetchCaseDetail(caseId) {
+  return instance.get(`case/${caseId}`)
+}
+
+// 사건경위서 사건 처리
+function acceptCase(caseData) {
+  return instance.put(`case/accept`,caseData)
+}
+
+
+
+export {
+  fetchCaseList,
+  fetchCaseDetail,
+  acceptCase
+}

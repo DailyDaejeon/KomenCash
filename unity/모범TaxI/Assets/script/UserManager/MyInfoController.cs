b365bcff1dd2f19f3e2e7acf77877f0f4000eb33
Hyerim Jeong <hrjeong8@gmail.com>
2021-05-07 17:25:18 +0900

    [UPDATE] update playerController

--- unity/모범TaxI/Assets/script/UserManager/MyInfoController.cs
+++ unity/모범TaxI/Assets/script/UserManager/MyInfoController.cs
@@ -15,7 +15,7 @@ public class MyInfoController : MonoBehaviour
     
     void Start()
     {
-        data = DataController.LoadUserInfo();
+        /*data = DataController.LoadUserInfo();
 
         Debug.Log("로그인 유저 정보 : ");
         data.print();
@@ -34,6 +34,6 @@ public class MyInfoController : MonoBehaviour
         }else
         {
             sCertList.text += "아직 보유중인 자격증이 없습니다ㅠ.ㅠ";
-        }
+        }*/
     }
 }

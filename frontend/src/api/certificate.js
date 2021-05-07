619a3e4c62b2fcd0657a669f8bb2d18aa879e5ff
박수아 <xlalfskdl@naver.com>
2021-05-07 12:51:02 +0900

    [ADD] teacher certification api

---
+++ frontend/src/api/certificate.js
@@ -0,0 +1,35 @@
+import { setInterceptors } from './config/interceptors'
+const instance = setInterceptors()
+
+// 자격증 정보 목록 조회
+function fetchCertiList(groupId) {
+  return instance.get(`certificate/group/${groupId}`)
+}
+
+// 자격증 정보 추가
+function addCertificate(certiData) {
+  return instance.post(`certificate`,certiData)
+}
+
+// 자격증 정보 수정
+function modifyCertificate(certiData) {
+  return instance.put(`certificate`,certiData)
+}
+
+// 자격증 정보 삭제
+function deleteCertificate(certiId) {
+  return instance.delete(`certificate/${certiId}`)
+}
+
+// 자격증 발급 요청 수락/거절
+function requestCertificate(certiData) {
+  return instance.put(`certificate/issue-request`,certiData)
+}
+
+export {
+  fetchCertiList,
+  addCertificate,
+  modifyCertificate,
+  deleteCertificate,
+  requestCertificate
+}

24989a3d7b336fdb310ed428f8edb103153c538a
배상웅 <dentist312@naver.com>
2021-05-11 13:34:48 +0900

    [Add] 은행원 - 학생들 월급 지급 하기 Closes S04P31B203-217

---
+++ backend/src/main/java/com/komencash/backend/dto/bank/SalaryPaymentRequestDto.java
@@ -0,0 +1,23 @@
+package com.komencash.backend.dto.bank;
+
+import com.komencash.backend.entity.request_history.Accept;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SalaryPaymentRequestDto {
+    private int id;
+    private int salary;
+    private int tax_loss;
+    private Accept accept;
+    private int student_id;
+
+    public SalaryPaymentRequestDto(int id, int student_id) {
+        this.id = id;
+        this.student_id = student_id;
+    }
+}
+

3b7f3e21d4c059e0996ba4c9bd95aaf1e69f8fa6
배상웅 <dentist312@naver.com>
2021-05-13 12:05:02 +0900

    [Update] Request Body int to Dto

--- backend/src/main/java/com/komencash/backend/dto/bank/SalaryRequestBodyDto.java
+++ backend/src/main/java/com/komencash/backend/dto/bank/SalaryRequestBodyDto.java
@@ -2,9 +2,11 @@ package com.komencash.backend.dto.bank;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SalaryRequestBodyDto {
     int data;
 }

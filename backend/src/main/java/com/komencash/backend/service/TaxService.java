2dbc5b5bd5131487eefe6e79d61bfff81f97adae
jaeseok-go <gojeasuk2@naver.com>
2021-05-13 14:43:54 +0900

    [MOD] update_inflation_api -> all_store_item_price_update

--- backend/src/main/java/com/komencash/backend/service/TaxService.java
+++ backend/src/main/java/com/komencash/backend/service/TaxService.java
@@ -1,11 +1,14 @@
 package com.komencash.backend.service;
 
+import com.komencash.backend.dto.store.StoreItemFindResponseDto;
 import com.komencash.backend.dto.tax.TaxHistoryAddUpdateRequestDto;
 import com.komencash.backend.dto.tax.TaxHistoryFindResponseDto;
 import com.komencash.backend.dto.tax.TaxRateUpdateRequestDto;
 import com.komencash.backend.entity.group.Group;
+import com.komencash.backend.entity.store.OnlineStoreItem;
 import com.komencash.backend.entity.tax.TaxHistory;
 import com.komencash.backend.repository.GroupRepository;
+import com.komencash.backend.repository.OnlineStoreItemRepository;
 import com.komencash.backend.repository.TaxHistoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +24,12 @@ public class TaxService {
 
     @Autowired
     GroupRepository groupRepository;
+
+    @Autowired
+    StoreService storeService;
+
+    @Autowired
+    OnlineStoreItemRepository onlineStoreItemRepository;
 
 
     public List<TaxHistoryFindResponseDto> findTaxHistoryList(int groupId){
@@ -63,6 +72,18 @@ public class TaxService {
     public boolean updateInflationRate(TaxRateUpdateRequestDto taxRateUpdateRequestDto){
         Group group = groupRepository.findById(taxRateUpdateRequestDto.getGroupId()).orElse(null);
         if(group == null) return false;
+
+
+        List<StoreItemFindResponseDto> storeItemFindResponseDtos = storeService.findStoreItemList(taxRateUpdateRequestDto.getGroupId());
+        storeItemFindResponseDtos.forEach(storeItemFindResponseDto -> {
+                OnlineStoreItem onlineStoreItem =
+                        new OnlineStoreItem(
+                                storeItemFindResponseDto.getId(),
+                                storeItemFindResponseDto.getName(),
+                                (int) (storeItemFindResponseDto.getPrice() * (1 + 0.01 * taxRateUpdateRequestDto.getTaxRate())),
+                                group);
+                onlineStoreItemRepository.save(onlineStoreItem);
+        });
 
         group.updateInflationRate(taxRateUpdateRequestDto.getTaxRate());
         groupRepository.save(group);

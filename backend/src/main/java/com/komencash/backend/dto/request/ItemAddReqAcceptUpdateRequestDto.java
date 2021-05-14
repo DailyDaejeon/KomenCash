8fc6d8d69d5cdf17362390495325cf1e3255f7ed
jaeseok-go <gojeasuk2@naver.com>
2021-05-15 00:23:12 +0900

    [MOD] store_controller_refactoring(code_convention)

---
+++ backend/src/main/java/com/komencash/backend/dto/request/ItemAddReqAcceptUpdateRequestDto.java
@@ -0,0 +1,15 @@
+package com.komencash.backend.dto.request;
+
+import com.komencash.backend.entity.request_history.Accept;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemAddReqAcceptUpdateRequestDto {
+
+    private int id;
+    private Accept accept;
+}
---
+++ backend/src/main/java/com/komencash/backend/dto/request/ItemAddReqFindResponseDto.java
@@ -0,0 +1,28 @@
+package com.komencash.backend.dto.request;
+
+import com.komencash.backend.entity.request_history.Accept;
+import com.komencash.backend.entity.request_history.OnlineStoreItemAddRequestHistory;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemAddReqFindResponseDto {
+
+    private int id;
+    private String name;
+    private Accept accept;
+    private int studentId;
+    private String studentNickname;
+
+    public ItemAddReqFindResponseDto(OnlineStoreItemAddRequestHistory onlineStoreItemAddRequestHistory) {
+        this.id = onlineStoreItemAddRequestHistory.getId();
+        this.name = onlineStoreItemAddRequestHistory.getName();
+        this.accept = onlineStoreItemAddRequestHistory.getAccept();
+        this.studentId = onlineStoreItemAddRequestHistory.getStudent().getId();
+        this.studentNickname = onlineStoreItemAddRequestHistory.getStudent().getNickname();
+    }
+
+}

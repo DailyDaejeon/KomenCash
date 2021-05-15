5334b902c7e8896205a151a191f685a52eaaa1f3
박수아 <xlalfskdl@naver.com>
2021-05-15 12:36:12 +0900

    [ADD] teacher router meta login closed S04P31B203-477 Actions (Type . to access issue actions)

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import store from '@/stores/index';
+
 Vue.use(Router);
 
 const router =  new Router({
@@ -41,27 +43,32 @@ const router =  new Router({
       path: '/group/main',
       name: 'MainPage',
       component: () => import('@/views/group/MainPage.vue'),
-      props:true
+      meta: { auth: true },
+      props: true
     },
     {
       path: '/group/list',
       name: 'GroupList',
+      meta: { auth: true },
       component: () => import('@/views/group/GroupList.vue'),
     },
     {
       path: '/group/job',
+      meta: { auth: true },
       name: 'JobPage',
       component: () => import('@/views/group/job/JobListPage.vue'),
     },
     {
       path: '/group/job/detail',
       name: 'JobDetailPage',
+      meta: { auth: true },
       component: () => import('@/views/group/job/JobDetailPage.vue'),
       props: true,
       children : [
         {
           path: ':dataName',
           name: 'JobDetail',
+          meta: { auth: true },
           component: () => import('@/components/group/job/JobDetail.vue'),
           props: true
         },
@@ -70,17 +77,20 @@ const router =  new Router({
     {
       path: '/group/job/resume',
       name: 'JobResumeRequest',
+      meta: { auth: true },
       component: () => import('@/components/group/job/JobRequestList.vue'),
     },
     {
       path: '/group/job/resume/detail',
       name: 'JobResumeDetail',
       props:true,
+      meta: { auth: true },
       component: () => import('@/components/group/job/Resume.vue')
     },
     {
       path: '/group/law',
       name: 'LawPage',
+      meta: { auth: true },
       component: () => import('@/views/group/law/LawPage.vue'),
       props: true,
       children:[
@@ -93,6 +103,7 @@ const router =  new Router({
         {
           path: ':id',
           name: 'LawType',
+          meta: { auth: true },
           component: () => import('@/components/group/law/LawType.vue'),
           props: true
         }
@@ -102,40 +113,47 @@ const router =  new Router({
       path: '/group/case',
       name: 'CasePage',
       component: () => import('@/views/group/law/CasePage.vue'),
+      meta: { auth: true },
       props: true
     },
     {
       path: '/group/vote/:id',
       name: 'VoteDetail',
       component: () => import('@/components/group/vote/VoteDetail.vue'),
+      meta: { auth: true },
       props: true
     },
     {
       path: '/group/tax',
       name: 'TaxPage',
+      meta: { auth: true },
       component: () => import('@/views/group/tax/TaxPage.vue'),
     },
     {
       path: '/group/bank/account',
       name: 'BankAccountPage',
+      meta: { auth: true },
       component: () => import('@/views/group/bank/BankAccountPage.vue'),
       children: [
         { 
-        path: ':id',
-        name: 'BankMemberDetail',
-        component: () => import('@/components/group/bank/BankMemberDetail.vue'),
-        props: true
+          path: ':id',
+          name: 'BankMemberDetail',
+          meta: { auth: true },
+          component: () => import('@/components/group/bank/BankMemberDetail.vue'),
+          props: true
         }
       ]
     },
     {
       path: '/group/bank/financial',
       name: 'BankFinancialPage',
+      meta: { auth: true },
       component: () => import('@/views/group/bank/BankFinancialList.vue'),
       children: [
         {
           path: ':id',
           name: 'BankFinancialDetail',
+          meta: { auth: true },
           component: () => import('@/components/group/bank/BankFinancialItem.vue'),
           props: true
         }
@@ -144,26 +162,31 @@ const router =  new Router({
     {
       path: '/group/bank/stock',
       name: 'StockPage',
+      meta: { auth: true },
       component: () => import('@/views/group/bank/StockPage.vue'),
     },
     {
       path: '/group/bank/stock/detail',
       name: 'StockDetailPage',
+      meta: { auth: true },
       component: () => import('@/views/group/bank/StockDetailPage.vue'),
       props: true
     },
     {
       path: '/group/credit/chart',
       name: 'CreditPage',
+      meta: { auth: true },
       component: () => import('@/views/group/credit/CreditPage.vue'),
     },
     {
       path: '/group/credit/statistic',
       name: 'StatisticPage',
+      meta: { auth: true },
       component: () => import('@/views/group/credit/StatisticPage.vue'),
       children: [{
         path: ':id',
         name: 'StatisticDetail',
+        meta: { auth: true },
         component: () => import('@/components/group/credit/GroupStatisticDetail.vue'),
         props: true
       }]
@@ -171,47 +194,55 @@ const router =  new Router({
     {
       path: '/group/store/products',
       name: 'StoreListPage',
+      meta: { auth: true },
       component: () => import('@/views/group/store/StoreListPage.vue'),
       props: true
     },
     {
       path: '/group/store/history',
       name: 'StoreHistoryPage',
+      meta: { auth: true },
       component: () => import('@/views/group/store/StoreHistoryPage.vue'),
       props: true
     },
     {
       path: '/group/setting',
       name: 'GroupSettingPage',
+      meta: { auth: true },
       component: () => import('@/views/group/GroupSettingPage.vue'),
       redirect: '/group/setting/groupInfo',
       children: [
         {
           path: 'groupInfo',
           name: 'GroupInfo',
+          meta: { auth: true },
           component: () => import('@/components/group/settings/GroupInfoForm.vue'),
           props: true
         }, {
           path: 'userInfo',
           name: 'UserInfoPage',
+          meta: { auth: true },
           component: () => import('@/components/group/settings/UserInfoPage.vue'),
           props: true
         },
         {
           path: 'certificate',
           name: 'GroupCertificate',
+          meta: { auth: true },
           component: () => import('@/components/group/settings/GroupCertificate.vue'),
           props: true
         },
         {
           path: 'groupMemberList',
           name: 'GroupMemberList',
+          meta: { auth: true },
           component: () => import('@/components/group/settings/GroupMemberListForm.vue'),
           props: true
         },
         {
           path: 'request',
           name: 'AddMemberRequest',
+          meta: { auth: true },
           component: () => import('@/components/group/settings/AddMemberRequestForm.vue'),
           props: true
         }
@@ -220,18 +251,39 @@ const router =  new Router({
     {
       path: '/group/member/',
       name: 'GroupMemberDetail',
+      meta: { auth: true },
       component: () => import('@/views/group/GroupMemberDetail.vue'),
       // redirect: '/group/member/groupInfo',
       children: [
         {
           path: ':id',
           name: 'GroupMemberInfo',
+          meta: { auth: true },
           component: () => import('@/components/group/settings/GroupMemberInfo.vue'),
           props: true
         }
       ]
     },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: () => import('@/views/NotFoundPage.vue'),
+      redirect: '/group/main'
+    },
   ],
 });
+
+  // console.log('로그인확인', store.state.isLogin)
+
+
+router.beforeEach((to, from, next) => {
+  // console.log('로그인확인', store.state.isLogin)
+  if (to.meta.auth && !store.state.user.isLogin) {
+    next('/member/login');
+    return;
+  }
+  next();
+});
+
 
 export default router;
---
+++ frontend/src/views/NotFoundPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Page를 찾을 수 없습니다.    
+  </div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>

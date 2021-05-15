8e234a733225059e732f06b9e87d0bddf12677b2
배상웅 <dentist312@naver.com>
2021-05-15 15:29:57 +0900

    [Update] 비밀번호 찾기할 때 Email로 ID 알아내기

--- backend/src/main/java/com/komencash/backend/controller/TeacherController.java
+++ backend/src/main/java/com/komencash/backend/controller/TeacherController.java
@@ -35,7 +35,7 @@ public class TeacherController {
     @ApiOperation(value = "이메일 중복 검사", notes = "입력받은 email이 선생님 정보에 존재하는 경우 false, 존재하지않는 경우 true를 반환")
     @ApiImplicitParam(name = "email", value = "email(이메일)", dataType = "String", required = true)
     @GetMapping("/dup-chk-email/{email}")
-    public boolean getTeacherByEmail(@PathVariable("email") String email) {
+    public int getTeacherByEmail(@PathVariable("email") String email) {
         return teacherService.findTeacherByEmail(email);
     }
 

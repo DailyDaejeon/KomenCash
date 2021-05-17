b673dbd94a4eeb83961a4004f17dbfd4f4da0d36
jaeseok-go <gojeasuk2@naver.com>
2021-05-17 20:46:39 +0900

    [MOD] download_type

--- backend/src/main/java/com/komencash/backend/controller/DownloadController.java
+++ backend/src/main/java/com/komencash/backend/controller/DownloadController.java
@@ -3,12 +3,15 @@ package com.komencash.backend.controller;
 import com.komencash.backend.service.DownloadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/download")
@@ -18,7 +21,7 @@ public class DownloadController {
     DownloadService downloadService;
 
     @GetMapping
-    public File executeFile(){
+    public Resource executeFile() throws IOException {
         return downloadService.executeFile();
     }
 
--- backend/src/main/java/com/komencash/backend/service/DownloadService.java
+++ backend/src/main/java/com/komencash/backend/service/DownloadService.java
@@ -1,19 +1,23 @@
 package com.komencash.backend.service;
 
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 
 @Service
 public class DownloadService {
 
-    public File executeFile(){
+    public Resource executeFile() throws IOException {
 
         String filePath = "/home/ubuntu/client-download-file/MoneyJam.zip";
-        return new File(filePath);
+        File file = new File(filePath);
+        InputStream is = FileUtils.openInputStream(file);
+        return new InputStreamResource(is);
     }
 }

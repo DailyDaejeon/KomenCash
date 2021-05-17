b10659c76a5462644173bd3f5a9be2cda56120dc
박수아 <xlalfskdl@naver.com>
2021-05-18 00:29:33 +0900

    [ADD] add teacher aboutpage alert student button

--- frontend/src/components/common/AboutPage.vue
+++ frontend/src/components/common/AboutPage.vue
@@ -5,7 +5,7 @@
     <h1 class="about1__h1">MONEYJAM
       <div class="about1__button">
         <button class="btn-2" @click="goTeacherPage">TEACHER</button>
-        <button class="btn-2">STUDENT</button>
+        <button class="btn-2" @click="downloadUnity">STUDENT</button>
       </div>
     </h1>
   </section>
@@ -24,6 +24,24 @@ export default {
     }
   },
   methods: {
+    downloadUnity() {
+      this.$swal({
+        title:'학생용 서비스를 받으시겠습니까?',
+        text : 'ZIP 파일로 받아지며, 알집 풀기 후 "유니티" 파일을 실행시키면 학생용 서비스를 이용하실 수 있습니다.',
+        icon:'question',
+        confirmButtonText:'Download',
+        showCancelButton:true
+      }).then((res)=>{
+        if (res.isConfirmed) {
+          // 다운받는 API
+          this.$swal({
+            title:'다운이 완료되었습니다.',
+            text:'유니티를 실행시켜주세요!',
+            icon:'success'
+          })
+        }
+      })
+    },
     goTeacherPage() {
       this.$router.push({name:'Login'})
     },

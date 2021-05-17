d3f1ec1b5744f06f1a3c5adf2e028352f1f75752
박수아 <xlalfskdl@naver.com>
2021-05-17 16:15:15 +0900

    [ADD] download unity file

--- frontend/src/components/common/AboutPage.vue
+++ frontend/src/components/common/AboutPage.vue
@@ -4,13 +4,21 @@
       <div class="grid__item color-7">
         <a class="link link--mallki" >MONEY JAM!<span data-letters="MONEY JAM!"></span><span data-letters="MONEY JAM!"></span></a>
       </div> -->
-    <div class="grid__item color-4">
+      <a class="btn bnt-main" href="/home/ubuntu/client-download-file/MoneyJam.zip" download>
+  Download
+</a>
+    <!-- <div class="grid__item color-4">
       <a class="link link--kumya"><span data-letters="MONEY JAM">MONEY JAM</span></a>
-    </div>
+    </div> -->
     <!-- </div> -->
   </section>
 </template>
 
+<script>
+export default {
+
+}
+</script>
 
 
 <style lang="scss" scoped>

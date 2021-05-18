2e444a90aff9c655931278eaa1fb82d92ebc91d2
박수아 <xlalfskdl@naver.com>
2021-05-18 16:12:56 +0900

    [UPDATE] modify about page swal closed

--- frontend/src/components/common/AboutPage.vue
+++ frontend/src/components/common/AboutPage.vue
@@ -33,13 +33,15 @@ export default {
         text : 'ZIP 파일로 받아지며, 알집 풀기 후 "유니티" 파일을 실행시키면 학생용 서비스를 이용하실 수 있습니다.',
         icon:'question',
         confirmButtonText:'Download',
-        showCancelButton:true
+        showCancelButton:true,
+
       }).then((res)=>{
         if (res.isConfirmed) {
           const studentBtn = document.querySelector('#student');
           studentBtn.href = "./MoneyJam.zip"
           studentBtn.download = 'MoneyJam.zip'
           studentBtn.click()
+          this.$swal.close()
           // 다운받는 API
           // this.$swal({
           //   title:'다운이 완료되었습니다.',
